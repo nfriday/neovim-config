ae0130141ce3ccdfa31a903ef3d8103263eca97d
Nicholas Friday <nicholas.friday@imanage.com>
2026-06-29 12:34:11 +0100

    dap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -95,6 +95,16 @@ return {
   },
 
   {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
+    opts = {
+      ensure_installed = { "python" },
+      handlers = {},
+    },
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
@@ -112,6 +122,47 @@ return {
         "python",
       },
     },
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+        config = function()
+          local dap, dapui = require "dap", require "dapui"
+          dapui.setup()
+          dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+          dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+          dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+        end,
+      },
+    },
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "DAP toggle breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "DAP continue" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "DAP step into" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "DAP step over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "DAP step out" },
+      { "<leader>dt", function() require("dap").terminate() end, desc = "DAP terminate" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "DAP toggle REPL" },
+      { "<leader>du", function() require("dapui").toggle() end, desc = "DAP toggle UI" },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = { "mfussenegger/nvim-dap" },
+    keys = {
+      { "<leader>dm", function() require("dap-python").test_method() end, desc = "DAP test method" },
+      { "<leader>dM", function() require("dap-python").test_class() end, desc = "DAP test class" },
+    },
+    config = function()
+      local mason_debugpy = vim.fn.stdpath "data" .. "/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(mason_debugpy)
+    end,
   },
 
   -- test new blink
