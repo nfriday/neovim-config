af128fbe7e1b1ebf95c44025a6b90ab6280a2aad
Nicholas Friday <nicholas.friday@imanage.com>
2026-07-14 10:35:28 +0100

    fix

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -122,9 +122,8 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
         "html",
         "css",
         "java",
@@ -134,7 +133,17 @@ return {
         "helm",
         "gotmpl",
         "python",
-      })
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter").setup(opts)
+      local installed = require("nvim-treesitter.config").get_installed()
+      local missing = vim.tbl_filter(function(p)
+        return not vim.list_contains(installed, p)
+      end, opts.ensure_installed or {})
+      if #missing > 0 then
+        require("nvim-treesitter").install(missing)
+      end
     end,
   },
 
